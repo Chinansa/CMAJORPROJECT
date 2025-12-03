103f3c47b5b6baa28589c5ddc2189fd3138196fb
Chinansa <rchinansa@gmail.com>
2025-12-03 15:44:00 +0530

    main.c

--- src/main.c
+++ src/main.c
@@ -1,1 +1,25 @@
 
+#include <stdio.h>
+#include "rental.h"
+
+int main() {
+    int choice, hours;
+    float bill;
+
+    displayMenu();
+    choice = getChoice();
+
+    if (choice == 1 || choice == 2) {
+        hours = getHours();
+        bill = calculateBill(choice, hours);
+        generateReceipt(choice, hours, bill);
+    }
+    else if (choice == 3) {
+        printf("Exiting the system...\n");
+    }
+    else {
+        printf("Invalid choice! Please try again.\n");
+    }
+
+    return 0;
+}
