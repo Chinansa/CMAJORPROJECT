923504409e29f343d8ba129b07e749bad009ab08
Chinansa <rchinansa@gmail.com>
2025-12-03 15:45:38 +0530

    header.h

--- include/header.h
+++ include/header.h
@@ -1,1 +1,12 @@
 
+#ifndef RENTAL_H
+#define RENTAL_H
+
+void displayMenu();
+int getChoice();
+int getHours();
+float calculateBill(int choice, int hours);
+void generateReceipt(int choice, int hours, float bill);
+
+#endif
+
